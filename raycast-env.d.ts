22b094e793c27f47d24998dd9f9fe1ea70be4910
Özenç Bilgili <18405305+ozencb@users.noreply.github.com>
2025-04-03 20:04:44 +0300

    Publish Raycast Obsidian Tasks extension

---
+++ raycast-env.d.ts
@@ -0,0 +1,55 @@
+/// <reference types="@raycast/api">
+
+/* 🚧 🚧 🚧
+ * This file is auto-generated from the extension's manifest.
+ * Do not modify manually. Instead, update the `package.json` file.
+ * 🚧 🚧 🚧 */
+
+/* eslint-disable @typescript-eslint/ban-types */
+
+type ExtensionPreferences = {
+  /** Tasks File In Your Vault - Path to your tasks file in your vault */
+  "filePath": string,
+  /** Show Due Date in Menubar - Display the due date of tasks in the menubar */
+  "showDueDate": boolean,
+  /** Show Task Count in Menubar - Show the number of tasks in the menubar */
+  "menubarTaskCount": boolean,
+  /** Show Icon in Menubar - Show the icon of tasks in the menubar */
+  "showIcon": boolean,
+  /** Sort by Priority - Sort tasks by priority */
+  "sortByPriority": boolean,
+  /** Max Description Length - Maximum length of task description to show in menubar */
+  "maxMenubarDescriptionLength": string,
+  /** Refresh Interval - Refresh interval in minutes */
+  "refreshIntervalInMinutes": string
+}
+
+/** Preferences accessible in all the extension's commands */
+declare type Preferences = ExtensionPreferences
+
+declare namespace Preferences {
+  /** Preferences accessible in the `list-tasks` command */
+  export type ListTasks = ExtensionPreferences & {}
+  /** Preferences accessible in the `add-task` command */
+  export type AddTask = ExtensionPreferences & {}
+  /** Preferences accessible in the `edit-task` command */
+  export type EditTask = ExtensionPreferences & {}
+  /** Preferences accessible in the `mark-done` command */
+  export type MarkDone = ExtensionPreferences & {}
+  /** Preferences accessible in the `menubar-item` command */
+  export type MenubarItem = ExtensionPreferences & {}
+}
+
+declare namespace Arguments {
+  /** Arguments passed to the `list-tasks` command */
+  export type ListTasks = {}
+  /** Arguments passed to the `add-task` command */
+  export type AddTask = {}
+  /** Arguments passed to the `edit-task` command */
+  export type EditTask = {}
+  /** Arguments passed to the `mark-done` command */
+  export type MarkDone = {}
+  /** Arguments passed to the `menubar-item` command */
+  export type MenubarItem = {}
+}
+
